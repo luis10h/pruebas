af826085f44485bdb9d1318e9fc6d3f40e60a447
Luis H <luisinfotv@gmail.com>
2025-05-16 20:39:01 -0500

    feat: agregar componente de cargador y servicio para manejar el estado de carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event, RouterOutlet } from '@angular/router';
+import { LoaderService } from './services/loader-service.service';
+import { LoaderComponent } from './pages/ui-components/cargando/loader.componet';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  imports: [
+    CommonModule,
+    LoaderComponent,
+    RouterOutlet
+  ],
 })
 export class AppComponent {
-  title = 'Modernize Angular Admin Tempplate';
+  title = 'Neo Company';
+  constructor(private router: Router, private loaderService: LoaderService) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.loaderService.show();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.loaderService.hide();
+      }
+    });
+  }
+  isLoading = this.loaderService.isLoading;
+
 }
---
+++ src/app/pages/ui-components/cargando/loader.componet.ts
@@ -0,0 +1,13 @@
+    import { Component } from '@angular/core';
+    import { RouterModule } from '@angular/router';
+    
+    @Component({
+      selector: 'app-loader',
+      standalone: true,
+      imports: [RouterModule],
+      templateUrl: './loader.component.html',
+      styleUrls: ['./loader.css']
+    })
+    export class LoaderComponent {
+
+    }
